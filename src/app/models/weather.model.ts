c52cd3514166256a3e1dbdcbefa84da13e9d5234
PeterF <piterdmc@gmail.com>
2023-10-23 13:24:56 +0200

    WeatherAppPartOne

---
+++ src/app/models/weather.model.ts
@@ -0,0 +1,4 @@
+export interface WeatherData {
+  //   coord: Coord
+  //   weather: Weather[]
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  weatherApiBaseUrl: 'https://weather-data10.p.rapidapi.com/data',
+  XRapidApiHostHeaderName: 'X-RapidAPI-Host',
+  XRapidApiHostHeaderValue: 'weather-data10.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: 'cf1b62a9c3msh24809125126e78ap10d80ajsn04161dc3fdb9',
+};
